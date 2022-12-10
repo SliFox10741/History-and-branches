54f3632cf9d08e64bf247f6c10cefee83dcef950
Maxim <mr.maximgus@gmail.com>
2022-12-10 23:17:11 +0500

    Inicial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long[] price = {13, 12, 10, 2, 31, 4};
+        SalesManager sale = new SalesManager(price);
+        long max = sale.max();
+        System.out.println("Максимальное значение - " + max);
+    }
+}
